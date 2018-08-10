73ca76a1075e6ae226c4fa150231b3ba174d3227
CrisEscobar <crisescobar0@hotmail.com>
2018-08-09 20:49:19 -0500

    corrigiendo detalles de css, agregando places

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-// Función para menú responsive
-const menuOptions = () => {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-
-
-var map;
-var infowindow;
+let map;
+let infowindow;
 
 //funcion que inicializa el mapa que tiene como parametro filter
 function initMap(searchValue) {  
@@ -21,9 +9,8 @@ function initMap(searchValue) {
     lat = pos.coords.latitude;
     lon = pos.coords.longitude;
 
-    var myLatlng = new google.maps.LatLng(lat, lon);  //objeto 
-
-    var mapOptions = {
+    let myLatlng = new google.maps.LatLng(lat, lon);  
+    let mapOptions = {
       center: myLatlng,
       zoom: 14,
       mapTypeId: google.maps.MapTypeId.MAP
@@ -31,25 +18,24 @@ function initMap(searchValue) {
 
     map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
 
-    // Creamos el infowindow
+   
     infowindow = new google.maps.InfoWindow();
 
-    // Especificamos la localización, el radio y el tipo de lugares que queremos obtener
-    var request = {
+    // request guarda el radio y el tipo de lugares que queremos obtener
+    let request = {
       location: myLatlng,
       radius: 5000,
       types: ["restaurant"]
     };
 
-    // Creamos el servicio PlaceService y enviamos la petición.
-    var service = new google.maps.places.PlacesService(map);
+    // Servicio PlaceService y enviamos la petición.
+    let service = new google.maps.places.PlacesService(map);
 
     service.nearbySearch(request, function (results, status) {
       
       if (status === google.maps.places.PlacesServiceStatus.OK) {
 
-        if (searchValue ) {  //si hay un filtro , filtra todo aquel cuyo nombre en minusculas que incluye al filtro en minusculas
-          //si el texto esta incliudo
+        if (searchValue ) {  
           results = results.filter(place => place.name.toLowerCase().includes(searchValue.toLowerCase()))
         }
         //la funcion updateList recibe los resultados y los pinta 
@@ -66,7 +52,7 @@ function initMap(searchValue) {
 
 function crearMarcador(place) {
   // Creamos un marcador
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
@@ -78,7 +64,7 @@ function crearMarcador(place) {
   });
 }
 
-//esta funcion
+//esta funcion jala los places
 function updateList(places) { 
 
   const postRestaurant = document.getElementById('postRestaurant');
@@ -86,33 +72,51 @@ function updateList(places) {
   for (let i = 0; i < places.length; i++) {
     const place = places[i]
     const contPlace = document.createElement('div');
-    contPlace.setAttribute('class', "w3-container w3-card w3-white w3-round w3-margin")
+    contPlace.setAttribute('class', "w3-container w3-card w3-white w3-round w3-margin-top w3-margin-bottom");
+    const space1 = document.createElement('br');
     contPlace.innerHTML = place.name
 
+    //Botón que abre el modal
     const buttonInformation = document.createElement("input");
     buttonInformation.setAttribute ('type', 'button')
-    buttonInformation.setAttribute ('value', 'Más información')
-    buttonInformation.setAttribute ('class', 'w3-button w3-black')
+    buttonInformation.setAttribute ('value', '+ Más información')
+    buttonInformation.setAttribute ('class', 'buttonInfo')
     buttonInformation.setAttribute ('style', 'display:block')
-    // buttonInformation.setAttribute ('onclick', "document.getElementById('id01').style.display='block'" )
     buttonInformation.onclick= () => {
       document.getElementById('id01').style.display='block';
       loadModal(place);
     }
+    const space2 = document.createElement('br');
+    
     postRestaurant.appendChild(contPlace);
+    contPlace.appendChild(space1);
     contPlace.appendChild(buttonInformation);
+    contPlace.appendChild(space2);
    
   }
 
 }
 
+//Función que jala y pinta la información para el modal. Tiene el parametro place.
 function loadModal (place) {
   const modalPlace = document.getElementById ("modalPlace")
-  modalPlace.innerHTML  = "<b>"+ place.name +"</b>";
-  modalPlace.innerHTML  += "<br>";
-  modalPlace.innerHTML+=   "Rating:" + place.rating ;
-  modalPlace.innerHTML  += "<br>";
-  modalPlace.innerHTML+=   "Dirección:" + place.vicinity;
+  modalPlace.innerHTML =  "<br>";
+  modalPlace.innerHTML += "<b>"+ place.name +"</b>";
+  modalPlace.innerHTML += "<br>";
+  modalPlace.innerHTML += "Rating: " + place.rating ;
+  modalPlace.innerHTML += "<br>";
+  modalPlace.innerHTML += "Dirección: " + place.vicinity;
+  modalPlace.innerHTML += "<br>";
+  if (place.opening_hours.open_now === false) {
+    local = 'Cerrado';
+  } else if (place.opening_hours.open_now === true) {
+    local = 'Abierto';
+  }
+  modalPlace.innerHTML += "Horario: " + local;
+	} 
 
 
-}
+ 
+  
+
+
